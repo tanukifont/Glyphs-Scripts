aec3f8988da58d0b56a8e5c7fd83fb070e076f07
tanukizamurai <kmk@tanukifont.com>
2024-10-20 13:41:41 +0900

    Update20241020

--- Text/Randomize selected characters and open in tab.py
+++ Text/Randomize selected characters and open in tab.py
@@ -13,18 +13,27 @@ Font = Glyphs.font
 
 # 選択されたグリフのstringをリスト形式で取得する
 def get_selected_glyphs_characters():
-	characters = []
-	for layer in Glyphs.font.selectedLayers:
-		glyph = layer.parent
-		characters.append("/" + glyph.name)
-	return characters
+    characters = []
+    for layer in Glyphs.font.selectedLayers:
+        glyph = layer.parent
+        characters.append("/" + glyph.name)
+    return characters
+
+# ユーザーにメッセージを表示する関数
+def show_message(message):
+    Glyphs.showNotification("スクリプトメッセージ", message)
+    print(message)  # マクロパネルにもメッセージを出力
 
 # 選択されたグリフのstringをリスト形式で取得する
 characters = get_selected_glyphs_characters()
 
-# 文字列をランダム生成
-random.shuffle(characters)
-rdmChr = ''.join(characters)
+if len(characters) == 0:
+    show_message("選択したグリフがありません。")
+else:
+    # 文字列をランダム生成
+    random.shuffle(characters)
+    rdmChr = ''.join(characters)
 
-Font.newTab(rdmChr)
-print("tab is opened.\n" + rdmChr)
+    # 新しいタブでランダムな文字列を開く
+    Font.newTab(rdmChr)
+    show_message("新しいタブが開かれました。\n" + rdmChr)
